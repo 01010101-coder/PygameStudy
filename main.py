618b3a90738eca515bd7c4136ba574ec921f1c05
vladyahnovets <v.yahnavets@gmail.com>
2022-06-01 15:51:33 +0300

    Добавляем игрока и убогие спрайты но в принципе работает

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 pygame.init() # запускаем pygame
 
 win = pygame.display.set_mode((500, 500)) # создаем окно и задаем ему размеры в px
@@ -11,13 +12,56 @@ x = 50
 y = 50
 # задаем размеры нашего объекта
 width = 40
-height = 60
+height = 37
 # задаем скорость передвижения нашего объекта
 speed = 5
 
 # создаем прыжок
 isJump = False
-jumpCount = 10
+jumpCount = 5
+
+# загружаем изображения
+
+walkRight = [pygame.image.load('png/walk_right_1.png'), pygame.image.load('png/walk_right_2.png'),
+             pygame.image.load('png/walk_right_3.png'), pygame.image.load('png/walk_right_4.png'),
+             pygame.image.load('png/walk_right_5.png'), pygame.image.load('png/walk_right_6.png')]
+
+
+
+walkLeft = [pygame.image.load('png/walk_left_1.png'), pygame.image.load('png/walk_left_2.png'),
+            pygame.image.load('png/walk_left_3.png'), pygame.image.load('png/walk_left_4.png'),
+            pygame.image.load('png/walk_left_5.png'), pygame.image.load('png/walk_left_6.png')]
+
+
+playerStand = pygame.image.load('png/idle (1).png')
+
+# двигается ли он? и в какую сторону
+left = False
+right = False
+animCount = 0
+
+def drawWindow():
+    global animCount
+
+    win.fill((255, 255, 255))  # а тут в конце мы просто делаем фон
+
+    if animCount + 1 >= 30:
+        animCount = 0
+
+    if left:
+        win.blit(walkLeft[animCount // 5], (x, y))
+        animCount += 1
+    elif right:
+        win.blit(walkRight[animCount//5], (x,y))
+        animCount += 1
+    else:
+        win.blit(playerStand, (x, y))
+
+
+    # если у нас есть кастомный фон, то
+    # win.blit(bg, (0,0 )), где bg - это наш фон, а (0,0) место откуда начать рисовать
+
+    pygame.display.update()  # обновляет окно
 
 
 run = True # переменная которая обозначает должна ли программа сейчас работать
@@ -38,27 +82,30 @@ while run:
     # кнопка, и если есть, то двигаем квадрат
     if keys[pygame.K_LEFT] and x > 5:
         x -= speed
-    if keys[pygame.K_RIGHT] and x < 500 - width - 5:
+        left = True
+        right = False
+    elif keys[pygame.K_RIGHT] and x < 500 - width - 5:
         x += speed
+        left = False
+        right = True
+    else:
+        left = False
+        right = False
+        animCount = 0
+    # при добавлении границ важно понимать, что отсчет у всего идет с левого верхнего угла
     if not(isJump):
-        if keys[pygame.K_UP] and y > 5:
-            y -= speed
-        if keys[pygame.K_DOWN] and y < 500 - height - 5:
-            y += speed
         if keys[pygame.K_SPACE]:
             isJump = True
     else:
-        if jumpCount >= -10:
+        if jumpCount >= -5:
             if jumpCount < 0:
                 y += (jumpCount ** 2) / 2
             else:
                 y -= (jumpCount ** 2) / 2
             jumpCount -= 1
-            print(x, y, jumpCount)
         else:
             isJump = False
-            jumpCount = 10
-            print(x, y)
+            jumpCount = 5
 
     # код для прыжка
     # Прыжок является параболлой, поэтому состоит из
@@ -66,16 +113,6 @@ while run:
     # падение начинается на середине прыжка, поэтому мы ждем
     # когда jumpCount станет на середине своего диапазона
     # и начинаем опускать
-
-
-
-    # при добавлении границ важно понимать, что отсчет у всего идет с левого верхнего угла
-    win.fill((0, 0, 0)) # а тут в конце мы просто делаем фон
-    # снова черным, чтобы квадрат не оставлял следов
-    # честно говоря весьма топорный способ и мне кажется есть по лучше
-    pygame.draw.rect(win, (255,255,255), (x, y, width, height)) # рисуем наш объект, в данном случае квадрат
-    # в начале идет, где мы его рисуем(наше окно), потом какого цвета(белый), а дальше его параметры(местоположение, размеры)
-    pygame.display.update() # обновляет окно
-
+    drawWindow()
 
 pygame.quit() # закрывает собственно окно
